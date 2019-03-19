19adb7621c09b32b46f46c7ad8f37b5e46d138fc
najayi <nifemia@gmail.com>
2019-03-18 21:27:33 -0400

    array of words per course

--- getWords.js
+++ getWords.js
@@ -6,7 +6,16 @@ var getWords = function(titles){
 	//  Filter out punctuation/numbers
 	//  Return words array
 	// -----------------------------------	
-
+	var words = [];
+	titles.forEach(text => {
+		text = text.toLowerCase();
+		text = text.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+		text = text.replace(/\W|\d/g, ' ');
+		text = text.replace(/\s+/g, ' ');
+		text = text.trim();
+		text = text.split(' ');
+		words.push(text);
+	});
 	return words;
 }
 
